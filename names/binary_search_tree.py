609928d0bc9e7b475cf6b816510bd3680e854e2d
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-18 09:44:17 +0100

    solve names

---
+++ names/binary_search_tree.py
@@ -0,0 +1,111 @@
+import sys
+sys.path.append('../queue_and_stack')
+
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if (self.left == None):
+                self.left = BinarySearchTree(value)
+
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.contains(target)
+
+        if target > self.value:
+            if self.right == None:
+                return False
+            else:
+                return self.right.contains(target)
+
+        # else:
+        #     return False
+
+    # Return the maximum value found in the tree
+
+    def get_max(self):
+        if self.value == None:
+            return None
+
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        if self.value != None:
+            cb(self.value)
+        if self.left != None:
+            self.left.for_each(cb)
+        if self.right != None:
+            self.right.for_each(cb)
+
+    # DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+
+    def in_order_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        q = Queue()
+        q.enqueue(self.value)
+        while q:
+            q.dequeue()
+            print(self.value)
+            if (self.left):
+                q.enqueue(self.left.value)
+            if (self.right):
+                q.enqueue(self.right.value)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        s = Stack()
+        s.push(self.value)
+        while s:
+            s.pop()
+            print(self.value)
+            if (self.left):
+                s.push(self.left.value)
+            if (self.right):
+                s.push(self.right.value)
+
+    # STRETCH Goals -------------------------
+    # Note: Research may be required
+
+    # Print In-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
--- names/names.py
+++ names/names.py
@@ -1,25 +1,28 @@
 import time
 import os
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
-firstPath = os.getcwd()+ '/names/names_1.txt'
+firstPath = os.getcwd() + '/names/names_1.txt'
 f = open(firstPath, 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-secondPath = os.getcwd()+ '/names/names_2.txt'
+secondPath = os.getcwd() + '/names/names_2.txt'
 f = open(secondPath, 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+tree = BinarySearchTree(names_1[0])
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    tree.insert(name_1)
+for name_2 in names_2:
+    if tree.contains(name_2):
+        duplicates.append(name_1)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
