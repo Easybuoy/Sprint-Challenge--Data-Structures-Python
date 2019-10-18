85fec2593d8e4d344b329804aef4b33d22eee7ed
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-18 08:56:57 +0100

    solve ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,37 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        if self.current < self.capacity:
+            self.storage[self.current] = item
+            self.current += 1
+        else:
+            if self.capacity == self.current:
+                self.storage[0] = item
+                self.current += 1
+            else:
+                self.storage[self.current - self.capacity] = item
+                self.current += 1
+            # del self.storage[0]
 
-  def get(self):
-    pass
+    def get(self):
+        return self.storage
+
+
+buffer = RingBuffer(5)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+buffer.append('f')
+buffer.append('g')
+buffer.append('h')
+buffer.append('i')
+print(buffer.get())   # should return ['a', 'b', 'c']
